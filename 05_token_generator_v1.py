bb75dc597e835e3b09e019a0d8ca25b859885f48
yewn2 <yewn2@middleton.school.nz>
2023-04-12 22:19:25 +1200

    token_generator version 1

---
+++ 05_token_generator_v1.py
@@ -0,0 +1,13 @@
+"""
+Component 3 (random tokens) v1
+Generates random choice of token in random order
+"""
+
+import random
+
+tokens = ["unicorn", "horse", "donkey", "zebra"]
+
+# Testing loop to generate 20 tokens
+for item in range (20):
+    token = random.choice(tokens)
+    print(token, end='\t') # can crop output making it easier to screenshot
