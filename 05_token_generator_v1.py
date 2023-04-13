b53e8875a0ef8da2bb6a52b68852c234b3a2dbd2
yewn2 <yewn2@middleton.school.nz>
2023-04-13 16:13:52 +1200

    token_generator version 1

--- 05_token_generator_v1.py
+++ 05_token_generator_v1.py
@@ -8,6 +8,6 @@ import random
 tokens = ["unicorn", "horse", "donkey", "zebra"]
 
 # Testing loop to generate 20 tokens
-for item in range (20):
+for item in range(20):
     token = random.choice(tokens)
-    print(token, end='\t') # can crop output making it easier to screenshot
+    print(token, end='\t')  # can crop output making it easier to screenshot
